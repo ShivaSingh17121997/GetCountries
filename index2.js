ae02e744969cb9e672e3d6361c50d84d8f0e4c72
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-02-22 10:00:28 +0530

    added

--- index2.js
+++ index2.js
@@ -21,27 +21,49 @@
 // uniqueElement(arr)
 
 
-const nums = [0,0,0,1,1,2,2,3,3,3,3]
+// const nums = [0, 0, 0, 1, 1, 2, 2, 3, 3, 3, 3]
 
 // function unique(arr) {
 
-//     let arr1 = [new Set(arr)]
+//     let arr1 = [...new Set(arr)]
 //     console.log(arr1)
 // }
 
 // unique(arr).
 
-function unique(nums) {
-    let uniquearr = []
-    let left = 1;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== nums[i - 1]) {
-            nums[left] = nums[i];
-            uniquearr.push(nums[left])
-            left++;
+// function unique(nums) {
+//     let uniquearr = []
+//     let left = 1;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== nums[i - 1]) {
+//             nums[left] = nums[i];
+//             uniquearr.push(nums[left])
+//             left++;
+//         }
+//     }
+//     console.log(uniquearr)
+// }
+
+// unique(nums)
+
+function discount(prices, discount) {
+    let max = -Infinity;
+    let sum = 0;
+
+    // Step 1: Find the maximum price
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] > max) {
+            max = prices[i];
         }
     }
-    console.log(uniquearr)
+
+    // Step 2: Apply discount only to the maximum price
+    let discountMax = max * (1 - discount / 100);
+
+    // Step 3: Calculate the sum with discount applied to the maximum price
+    sum = prices.reduce((acc, curr) => acc + (curr === max ? discountMax : curr), 0);
+
+    console.log(sum); // Output should be 620
 }
 
-unique(nums)
+discount([100, 200, 400], 20);
