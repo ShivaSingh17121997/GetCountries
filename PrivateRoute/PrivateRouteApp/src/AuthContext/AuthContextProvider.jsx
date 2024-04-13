584735546314517ca7ac0ca38a0a6e0e54edd4a8
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-04-13 10:48:14 +0530

    added'

--- PrivateRoute/PrivateRouteApp/src/AuthContext/AuthContextProvider.jsx
+++ PrivateRoute/PrivateRouteApp/src/AuthContext/AuthContextProvider.jsx
@@ -4,7 +4,7 @@ export const AuthContext = createContext();
 
 export default function AuthContextProvider({ children }) {
 
-    const [isAuth, setIsAuth] = useState(false);
+    const [isAuth, setIsAuth] = useState(true);
 
     const login = (a) => {
         setIsAuth(true);
--- PrivateRoute/PrivateRouteApp/src/Pages/About.jsx
+++ PrivateRoute/PrivateRouteApp/src/Pages/About.jsx
@@ -1,16 +1,49 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ThemeContexts } from '../AuthContext/ThemeContext'
 
 export default function About() {
     const { Theme, toggletheme } = useContext(ThemeContexts);
 
+    const [count, setCount] = useState(0);
+
+
+    JSON.stringify(localStorage.setItem("count", count));
+    const handleDecrease = () => {
+        setCount(prev => prev - 1)
+
+    };
+
+    const handleIncrease = () => {
+        setCount(prev => prev + 1)
+    };
+
+    const countVal = JSON.parse(localStorage.getItem("count"))
+
+    console.log(countVal)
+
+
     return (
-        <div style={{ height:"100vh", backgroundColor: Theme ? "white" : "black" }} >
+        <div style={{ height: "100vh", backgroundColor: Theme ? "white" : "black" }} >
+            <button onClick={toggletheme} >{Theme ? "Dark Mode" : "Light Mode"}</button>
             <div>
-                <h1  style={{text:"black"}} >  About</h1>
-                <button onClick={toggletheme} >{Theme ? "Dark Mode" : "Light Mode"}</button>
+                <h1 style={{ color: Theme ? "black" : "white" }} >  About</h1>
+                <div>
+                    <h1 style={{ color: Theme ? "black" : "white" }} >Counter app</h1>
+                    <p style={{ color: Theme ? "black" : "white" }} > localStorageVal{countVal}</p>
+                    <button disabled={count === 0} onClick={handleDecrease}  >Substract - </button>
+                    <h1 style={{ color: Theme ? "black" : "white" }} >{count}</h1>
+                    <button onClick={handleIncrease} >Addition+</button>
+
+                </div>
+
+
+
+
+
+
             </div>
 
         </div>
     )
 }
+;
